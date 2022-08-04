2be84dd1d6074957a98ff7732ea9c060c0b4a451
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-08-03 21:06:41 -0400

    Pair Programming - Word Search with Edgar Leung

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
+
+    let transposed = transpose(letters);
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)){
+            return true;
+        } 
     }
+    if (transposed.includes(word)){
+        return true;
+    } 
+    
+
+    return false;
 }
 
-module.exports = wordSearch
+const transpose = (arr) => {
+    for (let i =0; i < arr.length; i++) {
+        for(let j = 0; j < i; j++) {
+            let temp = arr[i][j]
+            arr[i][j] = arr[j][i]
+            arr[j][i] = temp;
+        }
+    }
+    
+    let verticalJoin = arr.map(ls => ls.join(''));
+    for (let i = 0; i < verticalJoin.length ; i++) {
+        if(verticalJoin[i] === '') {
+            verticalJoin.splice(i,1)
+        }
+    }
+    return verticalJoin;
+}
+
+module.exports = wordSearch;
